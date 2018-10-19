cc4c1c161f6e6596a38a9c46f59a3c9aa5cd1aca
safiulm123 <safiulhasan@gmail.com>
2018-10-19 12:22:11 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,7 @@ DOCKER_PASS="Test@1234"
 # docker login with username and password
 docker build -t safiultest/websvr:1.0 .
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS $DOCKER_HOST
+# Script pushes the websvr to docker hub
 docker push safiultest/websvr:1.0
+# start the container
 docker run -p 9000:8083 safiultest/websvr:1.0
